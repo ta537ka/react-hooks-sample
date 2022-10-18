2b10959f5a75b9a3644d2e120830e3de51f7e85b
ta537ka <ta64ka10@gmail.com>
2022-10-18 20:57:07 +0900

    create useEfect1

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import Basic1 from './components/Basic1';
 import Basic2 from './components/Basic2';
+import BasicUseEffect from './components/BasicUseEffect';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Basic1 name="Hooks" />
+        {/* <Basic1 name="Hooks" /> */}
         {/* <Basic1 name="REdux" /> */}
-        <Basic2 />
+        {/* <Basic2 /> */}
+        <BasicUseEffect />
       </header>
     </div>
   );
